2a41a8c24d58fbd9f0455f7384609f2ca3b509ce
EgorKancir <kancyregor@gmail.com>
2024-03-07 19:46:03 +0200

    Last commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 const iconMenu = document.querySelector('.menu-icon');
 if (iconMenu) {
+    const headerMenu = document.querySelector('.header');
     const menuBody = document.querySelector('.menu-body');
     iconMenu.addEventListener("click", function (e) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
+        headerMenu.classList.toggle('_active');
     });
 }
