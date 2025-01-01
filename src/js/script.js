7c089e1c656d62ce6df6c89bcdf3f606bfb26c00
EgorKancir <kancyregor@gmail.com>
2025-01-01 20:06:58 +0100

    Observer

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,44 @@ if (iconMenu) {
         headerMenu.classList.toggle('_active');
     });
 }
+
+// #Observer
+
+const decorIamge = document.querySelectorAll('.observer-image');
+
+const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.src = entry.target.dataset.src;
+            observer.unobserve(entry.target)
+        }
+    });
+}, {
+    rootMargin: '5px'
+});
+
+decorIamge.forEach(element => {
+    imageObserver.observe(element);
+});
+
+const allMainSection = document.querySelector('.main-observer');
+const allMainSectionArr = [...allMainSection.children];
+
+allMainSectionArr.forEach(element => {
+    element.classList.add('section-off');
+});
+
+const startSectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove('section-off');
+            entry.target.classList.add('section-on');
+            observer.unobserve(entry.target)
+        }
+    });
+}, {
+    threshold: 0.4
+});
+allMainSectionArr.forEach(element => {
+    startSectionObserver.observe(element);
+});
